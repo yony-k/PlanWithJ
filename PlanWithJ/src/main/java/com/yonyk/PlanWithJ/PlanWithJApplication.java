a4a760406fc244a50be94fe7f96c57b007844407
yony_k <kyh03179@gmail.com>
2024-03-26 15:26:15 +0900

    프로젝트 생성

---
+++ PlanWithJ/src/main/java/com/yonyk/PlanWithJ/PlanWithJApplication.java
@@ -0,0 +1,13 @@
+package com.yonyk.PlanWithJ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanWithJApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanWithJApplication.class, args);
+	}
+
+}
---
+++ PlanWithJ/src/test/java/com/yonyk/PlanWithJ/PlanWithJApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yonyk.PlanWithJ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanWithJApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
